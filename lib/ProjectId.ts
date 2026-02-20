79aaf2f82d16ec796757c6d40201f9ced0e7d096
ibrahimsaleh8 <ebrihm576@gmail.com>
2026-02-20 23:33:42 +0200

    change current URl

--- lib/ProjectId.ts
+++ lib/ProjectId.ts
@@ -4,4 +4,4 @@ export const CurrentProjectId = "cmlpdzx52000p1tnwgefj4yo4";
 // export const APP_URL = "http://localhost:5000";
 export const APP_URL = "https://qahwajige-backend-ppqtou.cranl.net";
 
-export const currentURL = "https://www.qahwery.com";
+export const currentURL = "https://www.qahwajie-alriyadh.com";
